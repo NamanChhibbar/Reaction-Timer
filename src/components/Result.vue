2af80e52dd51b9159eaf826db801f7564607b098
Naman Chhibbar <n.chhibar@gmail.com>
2023-07-02 14:58:34 +0530

    added a message for the user in result component

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,9 +1,22 @@
 <template>
-<div>Your reaction time: {{ props.result }}ms</div>
+<div>Your reaction time: {{ props.result }}ms<br>{{ message }}</div>
 </template>
 
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps(["result"]);
+const message = computed(() => {
+    if (props.result < 300) {
+        return "God level speed!";
+    } else if (props.result < 350) {
+        return "Quick reflexes!";
+    } else if (props.result < 400) {
+        return "Average";
+    } else {
+        return "Too slow";
+    }
+})
 </script>
 
 <style scoped>
